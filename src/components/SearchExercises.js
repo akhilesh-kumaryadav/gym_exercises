3518c16bb2deb0155e67693184ed4ecc0f30ce53
akhilesh-kumar-yadav <96679526+akhilesh-kumaryadav@users.noreply.github.com>
2023-06-10 16:38:20 +0530

    menu shown success

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,15 +4,14 @@ import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SearchExercises = () => {
+const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
   const [search, setsearch] = useState("");
-  const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
     const fetchExercisesData = async () => {
       const bodyPartsData = await fetchData(
-        "https://exercisedb.p.rapidapi.com/exercises",
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
         exerciseOptions
       );
 
@@ -84,7 +83,7 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{postion : 'relative', width:"100%", p:'20px'}}>
-          <HorizontalScrollbar data={bodyParts} />
+          <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} />
       </Box>
     </Stack>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,26 @@
-import React, {useState} from "react";
-import {Box} from '@mui/material';
+import React, { useState } from "react";
+import { Box } from "@mui/material";
 
-import Exercises from '../components/Exercises';
+import Exercises from "../components/Exercises";
 import SearchExercises from "../components/SearchExercises";
 import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
   );
 };
